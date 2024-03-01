82c32262bfdea4dc2c715c157947d09710eead9f
ramkyadav <ramkunwar.yadav@gmail.com>
2024-03-01 17:39:49 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -11,5 +11,5 @@ def print_hi(name):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    print_hi('Hell, This ia a github integration demo')
 
